7b106bff8ad142385782f72a00238e89bbec76e2
KusLukas <kusylu@gmail.com>
2024-03-03 17:57:09 +0100

    Improved loading of main window

--- src/main_window.py
+++ src/main_window.py
@@ -11,10 +11,27 @@ from PySide6.QtCore import QFile, QIODevice
 import read_data 
 
 
-# class SpecWindow(QMainWindow):
+class SpecWindow(QMainWindow):
 
-    # def __init__(self,parent=None):
-    #     pass
+    if __name__ == "__main__":
+        loader = QUiLoader()
+        app = QApplication(sys.argv)
+
+        ui_file_name = "UI_structure.ui"
+        ui_file = QFile(ui_file_name)
+        if not ui_file.open(QIODevice.ReadOnly):
+            print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+            sys.exit(-1)
+
+        window = loader.load(ui_file)
+        ui_file.close()
+        if not window:
+            print(loader.errorString())
+            sys.exit(-1)
+        window.show()
+
+        app.exec()
+
     # def __init__(self, *args, **kwargs):
     #     super().__init__(*args, **kwargs)
     #     self.title = "SpecApp"
@@ -44,27 +61,6 @@ import read_data
     #         self.title = "SpecApp"
     #     self.setWindowTitle(self.title)
         
-loader = QUiLoader()
-app = QApplication(sys.argv)
-window = loader.load("UI_structure.ui", None)
-window.show()
-app.exec()
 
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-#
-#     ui_file_name = "UI_structure.ui"
-#     ui_file = QFile(ui_file_name)
-#     if not ui_file.open(QIODevice.ReadOnly):
-#         print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
-#         sys.exit(-1)
-#
-#     loader = QUiLoader()
-#     window = loader.load(ui_file)
-#     ui_file.close()
-#     if not window:
-#         print(loader.errorString())
-#         sys.exit(-1)
-#     window.show()
-#
-#     sys.exit(app.exec())
+
+
